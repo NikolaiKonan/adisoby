921f3e07e1cef45473113caf3f86758bbfba884c
NikolaiKonan <qu1zzz1k@gmail.com>
2015-03-18 01:50:51 +0300

    Added one field

---
+++ AdisoAutomationProject/AdisoAutomationProject/Pages/AdvertisementPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdisoAutomationProject.Pages
+{
+    public class AdvertisementPage
+    {
+        private IWebDriver _driver;
+        public AdvertisementPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public string Category
+        {
+            set
+            {
+                var root = _driver.FindElement(By.Id("category_l1_chzn"));
+                root.Click();
+                var field = root.FindElement(By.XPath(".//div[@class='chzn-search']/input"));
+                field.SendKeys(value);
+                field.SendKeys(Keys.Enter);
+            }
+
+            get { return _driver.FindElement(By.Id("category_l1_chzn")).FindElement(By.XPath("./a")).Text; }
+        }
+    }
+}
--- AdisoAutomationProject/AdisoAutomationProject/Program.cs
+++ AdisoAutomationProject/AdisoAutomationProject/Program.cs
@@ -1,5 +1,9 @@
-﻿using AdisoAutomationProject.Utils;
+﻿using AdisoAutomationProject.Pages;
+using AdisoAutomationProject.Utils;
 using ClosedXML.Excel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,11 +16,23 @@ namespace AdisoAutomationProject
     {
         static void Main(string[] args)
         {
+            if (Process.GetProcesses().Select(p => p.ProcessName == "javaw").Count() > 0)
+            {
+                foreach (var process in Process.GetProcessesByName("javaw"))
+                { process.Kill(); }
+            }
             var server = Process.Start(string.Format("{0}\\selenium_server.jar", Environment.CurrentDirectory));
-            ExcelParser ep = new ExcelParser(@"D:\AdisoBy\adisoby\AdisoAutomationProject\AdisoAutomationProject\bin\Debug\1.xlsx");
+            ExcelParser ep = new ExcelParser(string.Format("{0}\\1.xlsx", Environment.CurrentDirectory));
             var a = ep.Values;
-          
 
+
+            //IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new RemoteWebDriver(DesiredCapabilities.HtmlUnitWithJavaScript());
+
+            driver.Navigate().GoToUrl(@"http://adiso.by/podat-obyavlenie");
+            AdvertisementPage ap = new AdvertisementPage(driver);
+            ap.Category = "Все для дома и быта";
+            var s = ap.Category;
             server.Kill();
         }
     }
